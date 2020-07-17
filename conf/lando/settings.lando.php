d50e5472fd8d4f125a9a282ff79137ccfa0d78d1
Brian Clement <brian@bclement.com>
2020-07-17 13:58:07 -0500

    Configure settings.php scaffolding.

---
+++ conf/lando/settings.lando.php
@@ -0,0 +1,12 @@
+<?php
+
+$databases['default']['default'] = [
+  'database' => 'drupal8',
+  'username' => 'drupal8',
+  'password' => 'drupal8',
+  'prefix' => '',
+  'host' => 'database',
+  'port' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+];
